87b4674f2fc6c0eb581ca6ef134cf98ea96aca85
JoyRume <joy.rume@student.moringaschool.com>
2024-01-06 18:02:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Add your code here
+
+const formData={
+    name: "Steve",
+    email: "steve@steve.com",
+}
+const submitData =(name,email)=> {
+    const configurationObject={
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(
+     formData
+    ),
+  };
+  
+   return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then((data)=> {
+    console.log(data);
+   document.body.innerHTML += data.id;
+   // document.body.innerHTML=data['id'];
+  })
+  .catch(function(error){
+//console.log(error.document.body.innerHTML)
+document.body.innerHTML += error.message;
+//document.body.innerHTML=error.message
+ })
+
+    // .catch( function ( error ) {
+    //   document.body.innerHTML = error.message;
+    //});
+
+}
+
